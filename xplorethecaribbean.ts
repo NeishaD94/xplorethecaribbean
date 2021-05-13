47c16890235a689317c17fa4eed4bea7f77bcd28
Lyneisha Dickenson <70129828+NeishaD94@users.noreply.github.com>
2021-05-13 03:53:09 -0400

    Add files via upload

---
+++ xplorethecaribbean.ts
@@ -0,0 +1,23 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
